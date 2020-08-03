fd5e74a3ce74d4a0286b486d005addec8c792d1f
황태영 <iamhwang9@gmail.com>
2020-08-04 02:12:35 +0900

    Test commit with setVersion branch

---
+++ src/App.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+import App from './App';
+import { MemoryRouter } from 'react-router';
+
+jest.mock('react-redux');  
+
+describe('App', () => {
+  const dispatch = jest.fn();
+  
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      
+    }));
+  });
+
+  it('renders App', () => {
+  const { container } = render((
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  ));
+
+  expect(container).toHaveTextContent('iamHwang');
+  });
+});
