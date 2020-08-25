e680999e345b66b5cc195dea65a0df8463b387eb
황태영 <iamhwang9@gmail.com>
2020-08-25 21:43:44 +0900

    Add html-webpack-plugin for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
@@ -19,4 +20,9 @@ module.exports = {
       index: 'index.html',
     },
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
 };
